d761f15f648bbe0b4543ea9d8abc7bb88b8ecd77
Noturno <Noturno@192.168.7.26>
2017-08-03 21:08:12 -0300

    commit agenda

---
+++ agenda/src/br/ufes/inf/prog3/lista2/exercicio04/dominio/ContatoEmail.java
@@ -0,0 +1,36 @@
+package br.ufes.inf.prog3.lista2.exercicio04.dominio;
+
+public class ContatoEmail implements Contato {
+
+	private String nome;
+	private String contato;
+	private String tipo;
+
+	@Override
+	public String getNome() {
+		return nome;
+	}
+
+	@Override
+	public String getContato() {
+		return contato;
+	}
+
+	@Override
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public void setContato(String contato) {
+		this.contato = contato;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+
+}
